90bc26c9aef1e2e445d6bd80cda132eed36344aa
Xavier Luxque <BUEXAL@degroofpetercam.com>
2021-07-08 11:33:36 +0200

    SHER-97

--- BDP.DPAM/4 - Web Resources/BDP.DPAM.WebResources/Script/JS/Account.js
+++ BDP.DPAM/4 - Web Resources/BDP.DPAM.WebResources/Script/JS/Account.js
@@ -7,11 +7,44 @@ var BDP;
         (function (WR) {
             var Account;
             (function (Account) {
+                class _Static {
+                    constructor() {
+                        this.field = {
+                            dpam_country: {
+                                dpam_s_alpha2code: "dpam_s_alpha2code"
+                            },
+                            account: {
+                                dpam_lk_country: "dpam_lk_country",
+                                dpam_s_country_alpha2code: "dpam_s_country_alpha2code"
+                            }
+                        };
+                    }
+                }
+                Account.Static = new _Static();
                 class Form {
                     static onLoad(executionContext) {
-                        const formContext = executionContext.getFormContext();
-                        const name = formContext.getAttribute("name").getValue();
-                        alert(`test ${name}`);
+                        /* const formContext: Xrm.FormContext = executionContext.getFormContext();
+             
+                         const name = formContext.getAttribute("name").getValue();
+                         alert(`test ${name}`);*/
+                    }
+                    static onChange_dpam_lk_country() {
+                        this.initDefaultCountryCode();
+                    }
+                    //function to initialize the field dpam_s_alpha2code based on dpam_lk_country
+                    static initDefaultCountryCode() {
+                        let _defaultPhoneCountryValue = "BE";
+                        let _country_attribute = Xrm.Page.getAttribute(Account.Static.field.account.dpam_lk_country);
+                        if (_country_attribute.getValue()
+                            && _country_attribute.getValue()[0]
+                            && _country_attribute.getValue()[0].id) {
+                            let _country_lookupvalue = _country_attribute.getValue()[0];
+                            Xrm.WebApi.retrieveRecord(_country_lookupvalue.entityType, _country_lookupvalue.id, `$select=${Account.Static.field.dpam_country.dpam_s_alpha2code}`).then(function (result) {
+                                _defaultPhoneCountryValue = result[Account.Static.field.dpam_country.dpam_s_alpha2code];
+                            }, function (error) { }).finally(function () {
+                                Xrm.Page.getAttribute(Account.Static.field.account.dpam_s_country_alpha2code).setValue(_defaultPhoneCountryValue);
+                            });
+                        }
                     }
                 }
                 Account.Form = Form;
--- BDP.DPAM/4 - Web Resources/BDP.DPAM.WebResources/Script/TS/Account.ts
+++ BDP.DPAM/4 - Web Resources/BDP.DPAM.WebResources/Script/TS/Account.ts
@@ -1,13 +1,59 @@
 ﻿/// <reference path="../../node_modules/@types/xrm/index.d.ts" />
 namespace BDP.DPAM.WR.Account {
 
+    class _Static {
+        readonly field = {
+            dpam_country: {
+                dpam_s_alpha2code: "dpam_s_alpha2code"
+            },
+            account: {
+                dpam_lk_country: "dpam_lk_country",
+                dpam_s_country_alpha2code: "dpam_s_country_alpha2code"
+            }
+        };
+
+    }
+    export let Static = new _Static();
+
+
     export class Form {
         public static onLoad(executionContext: Xrm.Events.EventContext): void {
 
-            const formContext: Xrm.FormContext = executionContext.getFormContext();
+            /* const formContext: Xrm.FormContext = executionContext.getFormContext();
+ 
+             const name = formContext.getAttribute("name").getValue();
+             alert(`test ${name}`);*/
+        }
+        public static onChange_dpam_lk_country() {
 
-            const name = formContext.getAttribute("name").getValue();
-            alert(`test ${name}`);
+            this.initDefaultCountryCode();
+
+        }
+
+        //function to initialize the field dpam_s_alpha2code based on dpam_lk_country
+        static initDefaultCountryCode() {
+
+            let _defaultPhoneCountryValue: string = "BE";
+            let _country_attribute: Xrm.Page.LookupAttribute = Xrm.Page.getAttribute<Xrm.Page.LookupAttribute>(Static.field.account.dpam_lk_country);
+
+
+            if (_country_attribute.getValue()
+                && _country_attribute.getValue()[0]
+                && _country_attribute.getValue()[0].id) {
+
+                let _country_lookupvalue = _country_attribute.getValue()[0];
+                Xrm.WebApi.retrieveRecord(_country_lookupvalue.entityType, _country_lookupvalue.id,
+                    `$select=${Static.field.dpam_country.dpam_s_alpha2code}`
+
+                ).then(function (result) {
+                    _defaultPhoneCountryValue = result[Static.field.dpam_country.dpam_s_alpha2code];
+                },
+                    function (error) { }
+
+                ).finally(function () {
+                    Xrm.Page.getAttribute(Static.field.account.dpam_s_country_alpha2code).setValue(_defaultPhoneCountryValue);
+                });
+            }
         }
     }
 }
