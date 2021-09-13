72e9cb90759e296c79cf95dce9b2c9338e6b8b41
Vincent Belin <vzb@degroofpetercam.com>
2021-09-13 10:53:47 +0200

    code refactory

--- BDP.DPAM/2 - Plugins/BDP.DPAM.Plugins.Account/Controller/AccountController.cs
+++ BDP.DPAM/2 - Plugins/BDP.DPAM.Plugins.Account/Controller/AccountController.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using BDP.DPAM.Shared.Extension_Methods;
 using BDP.DPAM.Shared.Manager_Base;
 using Microsoft.Xrm.Sdk;
-using Microsoft.Xrm.Sdk.Query;
+
 
 namespace BDP.DPAM.Plugins.Account.Controller
 {
@@ -20,28 +16,19 @@ namespace BDP.DPAM.Plugins.Account.Controller
 
         internal void CompleteSegmentation()
         {
-            if (!this._target.Contains("dpam_lk_localbusinesssegmentation"))
+            if (!this._target.Contains("dpam_lk_localbusinesssegmentation") || _context.MessageName != MessageName.Update)
                 return;
-
             this._tracing.Trace("CompleteSegmentation on Account - Start");
-
-            Entity mergedAccount = this._target.MergeEntity(this._preImage);
-
-            var localSegmentationEntityReference = _target.GetAttributeValue<EntityReference>("dpam_lk_localbusinesssegmentation");
-
-            if (localSegmentationEntityReference == null && _context.MessageName == MessageName.Update)
+            if (this._target["dpam_lk_localbusinesssegmentation"] != null)
             {
-                _tracing.Trace("CompleteSegmentation on Account - End");
-                return;
+                Entity localSegmentationEntity = _service.Retrieve(_target.GetAttributeValue<EntityReference>("dpam_lk_localbusinesssegmentation"), "dpam_lk_businesssegmentation");
+
+                this._target["dpam_lk_businesssegmentation"] = localSegmentationEntity.GetAttributeValue<EntityReference>("dpam_lk_businesssegmentation");
             }
-
-            var localSegmentationEntity = new Entity("dpam_localbusinesssegmentation");
-            if (localSegmentationEntityReference != null)
+            else
             {
-                var columnSet = new ColumnSet("dpam_lk_businesssegmentation");
-                localSegmentationEntity = _service.Retrieve(localSegmentationEntityReference.LogicalName, localSegmentationEntityReference.Id, columnSet);
-                var segmentation = _service.Retrieve("dpam_counterpartybusinesssegmentation", localSegmentationEntity.GetAttributeValue<EntityReference>("dpam_lk_businesssegmentation").Id, new ColumnSet("dpam_counterpartybusinesssegmentationid"));
-                this._target["dpam_lk_businesssegmentation"] = segmentation.ToEntityReference();
+                this._target["dpam_lk_businesssegmentation"] = null;
+
             }
             this._tracing.Trace("CompleteSegmentation on Account - End");
         }
