7be7714116bec4c18ba29c86c6795e856d4b87ba
Lindsay Berger <l.berger@degroofpetercam.com>
2021-09-22 09:18:07 +0200

    Plugin Account: Remove this. and the check on the MessageName

--- BDP.DPAM/2 - Plugins/BDP.DPAM.Plugins.Account/Controller/AccountController.cs
+++ BDP.DPAM/2 - Plugins/BDP.DPAM.Plugins.Account/Controller/AccountController.cs
@@ -6,31 +6,36 @@ using Microsoft.Xrm.Sdk;
 
 namespace BDP.DPAM.Plugins.Account.Controller
 {
-    class AccountController : PluginManagerBase
+    internal class AccountController : PluginManagerBase
     {
 
         internal AccountController(IServiceProvider serviceProvider) : base(serviceProvider)
         {
         }
 
-
+        /// <summary>
+        /// Fill in the field dpam_lk_businesssegmentation
+        /// </summary>
         internal void CompleteSegmentation()
         {
-            if (!this._target.Contains("dpam_lk_localbusinesssegmentation") || _context.MessageName != MessageName.Update)
+            if (!_target.Contains("dpam_lk_localbusinesssegmentation"))
                 return;
-            this._tracing.Trace("CompleteSegmentation on Account - Start");
-            if (this._target["dpam_lk_localbusinesssegmentation"] != null)
+
+            _tracing.Trace("CompleteSegmentation - Start");
+
+            if (_target["dpam_lk_localbusinesssegmentation"] != null)
             {
                 Entity localSegmentationEntity = _service.Retrieve(_target.GetAttributeValue<EntityReference>("dpam_lk_localbusinesssegmentation"), "dpam_lk_businesssegmentation");
 
-                this._target["dpam_lk_businesssegmentation"] = localSegmentationEntity.GetAttributeValue<EntityReference>("dpam_lk_businesssegmentation");
+                _target["dpam_lk_businesssegmentation"] = localSegmentationEntity.GetAttributeValue<EntityReference>("dpam_lk_businesssegmentation");
             }
             else
             {
-                this._target["dpam_lk_businesssegmentation"] = null;
+                _target["dpam_lk_businesssegmentation"] = null;
 
             }
-            this._tracing.Trace("CompleteSegmentation on Account - End");
+
+            _tracing.Trace("CompleteSegmentation - End");
         }
     }
 }
--- BDP.DPAM/2 - Plugins/BDP.DPAM.Plugins.Account/PreUpdateAccount.cs
+++ BDP.DPAM/2 - Plugins/BDP.DPAM.Plugins.Account/PreUpdateAccount.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using BDP.DPAM.Plugins.Account.Controller;
 using BDP.DPAM.Shared.Manager_Base;
 using Microsoft.Xrm.Sdk;
