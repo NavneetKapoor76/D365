121cb7da7bb8b0cd839dc3c8416183742e66a698
Lindsay Berger <l.berger@degroofpetercam.com>
2021-09-22 10:05:08 +0200

    Plugin Account: Add unit tests

---
+++ BDP.DPAM/2 - Plugins/BDP.DPAM.Plugins.Account.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BDP.DPAM.Plugins.Account.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BDP.DPAM.Plugins.Account.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e2b3b1d0-3862-4514-ac5b-c93cb9f3b853")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
