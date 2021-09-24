4551cc9dae424b093e21a7dd7579e72e4d8760c3
Lindsay Berger <l.berger@degroofpetercam.com>
2021-09-24 12:43:51 +0200

    SHER-324: Local business location at the department creation

---
+++ BDP.DPAM/2 - Plugins/BDP.DPAM.Plugins.Department/PreCreateDepartment.cs
@@ -0,0 +1,25 @@
+﻿using BDP.DPAM.Shared.Manager_Base;
+using Microsoft.Xrm.Sdk;
+using System;
+
+namespace BDP.DPAM.Plugins.Department
+{
+    public class PreCreateDepartment : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            try
+            {
+                DepartmentController ctrl = new DepartmentController(serviceProvider);
+                ctrl.ValidatePipeline("dpam_departments", "create", PluginStage.PreOperation);
+                //SHER-324
+                ctrl.FillInBusinessSegmentation();
+                
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidPluginExecutionException(ex.Message);
+            }
+        }
+    }
+}
