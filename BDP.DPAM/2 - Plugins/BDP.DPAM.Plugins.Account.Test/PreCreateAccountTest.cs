d33eacaed24795a945ed4ea340f9ebc8809b12fe
Vincent Belin <vzb@degroofpetercam.com>
2021-10-05 16:12:23 +0200

    Test on SHER-344

--- BDP.DPAM/2 - Plugins/BDP.DPAM.Plugins.Account.Test/PreCreateAccountTest.cs
+++ BDP.DPAM/2 - Plugins/BDP.DPAM.Plugins.Account.Test/PreCreateAccountTest.cs
@@ -70,5 +70,64 @@ namespace BDP.DPAM.Plugins.Account.Test
             Assert.Equal($"{street}, {postalCode}, {city}, {countryName}", accountTarget.GetAttributeValue<string>("dpam_s_address1"));
             Assert.Equal(countryName, accountTarget.GetAttributeValue<string>("address1_country"));
         }
+
+        [Fact]
+        public void CompleteSegmentation_Set_Business_Segmentation_Based_On_Local_Business_Segmentation()
+        {
+            var fakeContext = new XrmFakedContext();
+            var entityList = new List<Entity>();
+
+            var businessSegmentation = new Entity("dpam_counterpartybusinesssegmentation")
+            {
+                Id = Guid.NewGuid(),
+                Attributes =
+                {
+                    {"dpam_s_name", "Asset Managers" }
+                }
+            };
+            entityList.Add(businessSegmentation);
+
+            var localBusinessSegmentation = new Entity("dpam_cplocalbusinesssegmentation")
+            {
+                Id = Guid.NewGuid(),
+                Attributes =
+                {
+                    {"dpam_s_name", "Test Local Business Segmentation" },
+                    {"dpam_lk_businesssegmentation", businessSegmentation.ToEntityReference() }
+                }
+            };
+            entityList.Add(localBusinessSegmentation);
+
+            fakeContext.Initialize(entityList);
+
+            var counterpartyTarget = new Entity("account")
+            {
+                Id = Guid.NewGuid(),
+                Attributes =
+                {
+                    {"dpam_lk_localbusinesssegmentation", localBusinessSegmentation.ToEntityReference() },
+                    {"dpam_lk_businesssegmentation", null }
+                }
+            };
+
+            var inputParameters = new ParameterCollection
+            {
+                {"Target", counterpartyTarget }
+            };
+
+            var executionFakeContext = new XrmFakedPluginExecutionContext()
+            {
+                InputParameters = inputParameters,
+                PreEntityImages = new EntityImageCollection(),
+                PostEntityImages = new EntityImageCollection(),
+                SharedVariables = new ParameterCollection(),
+                MessageName = "Create",
+                Stage = (int)PluginStage.PreOperation
+            };
+
+            fakeContext.ExecutePluginWith<PreCreateAccount>(executionFakeContext);
+
+            Assert.Equal(localBusinessSegmentation.GetAttributeValue<EntityReference>("dpam_lk_businesssegmentation")?.Id, counterpartyTarget.GetAttributeValue<EntityReference>("dpam_lk_businesssegmentation")?.Id);
+        }
     }
 }
