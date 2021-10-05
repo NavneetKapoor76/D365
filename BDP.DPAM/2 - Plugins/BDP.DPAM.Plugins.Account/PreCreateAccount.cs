3cfe5839bd3a7c8b3fd27f4f4dc10d7dfbaf266b
Vincent Belin <vzb@degroofpetercam.com>
2021-10-05 15:29:17 +0200

    Sher-344 initaila revision

---
+++ BDP.DPAM/2 - Plugins/BDP.DPAM.Plugins.Account/PreCreateAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BDP.DPAM.Plugins.Account.Controller;
+using BDP.DPAM.Shared.Manager_Base;
+using Microsoft.Xrm.Sdk;
+
+namespace BDP.DPAM.Plugins.Account
+{
+    public class PreCreateAccount : IPlugin
+    {
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            try
+            {
+                AccountController ctrl = new AccountController(serviceProvider);
+                ctrl.ValidatePipeline("account", "create", PluginStage.PreOperation);
+                //SHER-344
+                ctrl.CompleteSegmentation();
+                
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidPluginExecutionException(ex.Message);
+            }
+        }
+
+    }
+}
