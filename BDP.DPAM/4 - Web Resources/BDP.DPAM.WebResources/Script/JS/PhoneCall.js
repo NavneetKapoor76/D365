a2298fc7824e489496b286e8c165db95237d9136
Robert Cavas <RC7809@degroofpetercam.com>
2021-12-22 10:20:19 +0100

    SHER-660

---
+++ BDP.DPAM/4 - Web Resources/BDP.DPAM.WebResources/Script/JS/PhoneCall.js
@@ -0,0 +1,40 @@
+/// <reference path="../../node_modules/@types/xrm/index.d.ts" />
+var BDP;
+(function (BDP) {
+    var DPAM;
+    (function (DPAM) {
+        var WR;
+        (function (WR) {
+            var PhoneCall;
+            (function (PhoneCall) {
+                class Form {
+                    static onLoad(executionContext) {
+                        const formContext = executionContext.getFormContext();
+                        //SHER-660
+                        Form.setDueDateToTodayDate(formContext);
+                        //SHER-660
+                        Form.setDefaultDuration(formContext);
+                    }
+                    static quickCreateOnLoad(executionContext) {
+                        const formContext = executionContext.getFormContext();
+                        //SHER-660
+                        Form.setDueDateToTodayDate(formContext);
+                        //SHER-660
+                        Form.setDefaultDuration(formContext);
+                    }
+                    //Set "scheduldend" (Due Date) field to today's date by default at creation.
+                    static setDueDateToTodayDate(formContext) {
+                        var currentDate = new Date();
+                        formContext.getAttribute("scheduledend").setValue(currentDate);
+                    }
+                    //Set "actualdurationminutes" (Duration) field to 15 minutes by default at creation.
+                    static setDefaultDuration(formContext) {
+                        formContext.getAttribute("actualdurationminutes").setValue(15);
+                    }
+                }
+                PhoneCall.Form = Form;
+            })(PhoneCall = WR.PhoneCall || (WR.PhoneCall = {}));
+        })(WR = DPAM.WR || (DPAM.WR = {}));
+    })(DPAM = BDP.DPAM || (BDP.DPAM = {}));
+})(BDP || (BDP = {}));
+//# sourceMappingURL=PhoneCall.js.map
