3df8826c63ff2e710a4922dc116ae460b24ae789
Gonzalo Enterrios <GE0370@degroofpetercam.com>
2022-04-04 09:52:23 +0000

    Added Make Service Principal PowerApp Manager.ps1 to /Solutions

---
+++ Solutions/Make Service Principal PowerApp Manager.ps1
@@ -0,0 +1,3 @@
+﻿Install-Module -Name Microsoft.PowerApps.Administration.PowerShell
+Install-Module -Name Microsoft.PowerApps.PowerShell -AllowClobber
+New-PowerAppManagementApp -ApplicationId cd153816-8b08-45b1-84b4-3c1071803488
