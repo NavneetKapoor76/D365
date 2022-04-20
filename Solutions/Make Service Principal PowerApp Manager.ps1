bf6fa5bb7733f5d812a2efc24f2903934f0c5a34
Gonzalo Enterrios <GE0370@degroofpetercam.com>
2022-04-20 10:53:08 +0000

    Deleted Make Service Principal PowerApp Manager.ps1

--- Solutions/Make Service Principal PowerApp Manager.ps1
+++
@@ -1,3 +0,0 @@
-﻿Install-Module -Name Microsoft.PowerApps.Administration.PowerShell
-Install-Module -Name Microsoft.PowerApps.PowerShell -AllowClobber
-New-PowerAppManagementApp -ApplicationId cd153816-8b08-45b1-84b4-3c1071803488
