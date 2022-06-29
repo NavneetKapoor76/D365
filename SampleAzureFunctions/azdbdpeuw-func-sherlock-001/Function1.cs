b17d3aeed8cab999af2a759fa03e545af9db228f
Navneet Kapoor <n.kapoor@degroofpetercam.com>
2022-06-29 13:10:04 +0000

    Updated Function1.cs

--- SampleAzureFunctions/azdbdpeuw-func-sherlock-001/Function1.cs
+++ SampleAzureFunctions/azdbdpeuw-func-sherlock-001/Function1.cs
@@ -10,14 +10,35 @@ using Newtonsoft.Json;
 
 namespace SampleFunctionApp
 {
-    public static class Function1
+      public static class Vehicle
     {
-        [FunctionName("Function1")]
-        public static async Task<IActionResult> Run(
+        [FunctionName("FunctionAUDI")]
+        public static async Task<IActionResult> AUDI(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("FunctionAUDI processed a request.");
+            //Add the Business Logic
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This FunctionAUDI executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This FunctionAUDI executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+
+        [FunctionName("FunctionBMW")]
+        public static async Task<IActionResult> BMW(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("FunctionBMW processed a request.");
+            //Add the Business Logic
 
             string name = req.Query["name"];
 
@@ -26,8 +47,28 @@ namespace SampleFunctionApp
             name = name ?? data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello Test :, {name}. This HTTP triggered function executed successfully.";
+                ? "This FunctionBMW executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This FunctionBMW executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+        [FunctionName("FunctionSkoda")]
+        public static async Task<IActionResult> Skoda(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("FunctionSkoda processed a request.");
+
+            //Add the Business Logic
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This FunctionSkoda executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This FunctionSkoda executed successfully.";
 
             return new OkObjectResult(responseMessage);
         }
